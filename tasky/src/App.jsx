a0aa39dd44f9b36740629543da5d0214ad58cf28
JiaruiXU0309 <202383930026@nuist.edu.cn>
2026-09-16 10:37:18 +0100

    Added task state

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -5,15 +5,24 @@ import viteLogo from './assets/vite.svg'
 import './App.css'
 import Task from './components/Task'
 
+
 function App() {
   const [count, setCount] = useState(0)
+  const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { title: "Tidy up", deadline: "Today" }
+    ]
+  });
+
 
   return (
     <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" description="Empty dishwasher"/>
-      <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put away"/>
-      <Task title="Tidy" deadline="Today" />
+       <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
+      <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
+      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
     </div>
   )
 }
